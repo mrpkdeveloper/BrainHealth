88544a8641424af29edc7fedeb4ce64d3ddb861f
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-20 22:55:13 +0530

    username displayed on navbar

--- src/public/app/common.js
+++ src/public/app/common.js
@@ -1,6 +1,24 @@
 $(
     () => {
-        $('#navbar').load('/components/navbar.html')
+        //after loading navbar.html file than obly loginifneeded function will work
+        $('#navbar').load('/components/navbar.html', loginIfNeeded)
         $('#footer').load('/components/footer.html')
+
     }
 )
+
+function loginIfNeeded() {
+    window.currentuser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null
+    if (!currentuser) {
+        $.post('/api/users', {}, (user) => {
+            console.log("current user as  : ", user.username)
+            window.localStorage.user = JSON.stringify(user)
+            currentuser = user
+            $('#nav-username').text(currentuser.username)
+
+        })
+    } else {
+        console.log("resuming session as ", currentuser.username)
+        $('#nav-username').text(currentuser.username)
+    }
+}

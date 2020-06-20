054b8bedccd249c3d641c073b82f6338bafc400b
Varun <varunbhouria78@gmail.com>
2020-06-20 13:58:14 +0530

    basic routes

---
+++ src/routes/articles/comments.js
@@ -0,0 +1,7 @@
+const { Router } = require('express')
+
+const route = Router()
+
+module.exports = {
+   commentsroute: route
+}
---
+++ src/routes/users/index.js
@@ -0,0 +1,11 @@
+const { Router } = require('express')
+
+const route = Router()
+
+route.get('/',(req,res)=>{
+   res.send('dasdadasdas')
+})
+
+module.exports = {
+   usersroute: route
+}

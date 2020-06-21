ae3a5a7e1dd4cb3318b043f12b195915e6312f20
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-21 08:23:24 +0530

    added myarticles section

---
+++ src/public/app/myarticles.js
@@ -0,0 +1,29 @@
+function loadMyArticles() {
+    let id = currentuser.id
+    $.get(`/api/articles/${id}`, (posts) => {
+        for (let p of posts) {
+            // console.log(p)
+            $('#Article-container').append(
+                $(`
+            <div class="col-4">
+                <div class="card m-2" style="width: 18rem;">
+                <img src="#" class="card-img-top" alt="...">
+                 <div class="card-body">
+                    <h5 class="card-title">${p.title}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${currentuser.username}</h6>
+                    <p class="card-text">${p.body.substring(0, 100)}
+                    <a href="#">....Readmore</a>
+                    </p>
+                    <a href="#" class="card-link">Comments</a>
+                    <a href="#" class="card-link">Like</a>
+                 </div>
+                </div>        
+            </div>
+        
+        `)
+            )
+
+
+        }
+    })
+}

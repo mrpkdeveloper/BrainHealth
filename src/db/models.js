93203f892cacf0cbda465bdbffcee4ef222aae8f
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2021-08-05 22:25:10 +0530

    fixed vulnerablities

--- src/db/models.js
+++ src/db/models.js
@@ -1,9 +1,8 @@
-const Sequelize = require('sequelize')
+const Sequelize = require("sequelize");
 
 const db = new Sequelize(process.env.DATABASE_URL, {
-   dialect: 'postgres'
-
-})
+  dialect: "postgres",
+});
 
 // const db = new Sequelize({
 //    dialect: 'mysql',
@@ -12,55 +11,64 @@ const db = new Sequelize(process.env.DATABASE_URL, {
 //    password: 'healthpass'
 // })
 
-const COL_ID_DEF = {                      //general column id
-   type: Sequelize.DataTypes.INTEGER,
-   autoIncrement: true,
-   primaryKey: true
-}
+const COL_ID_DEF = {
+  //general column id
+  type: Sequelize.DataTypes.INTEGER,
+  autoIncrement: true,
+  primaryKey: true,
+};
 
-const COL_TITLE_DEF = {                   //general column title
-   type: Sequelize.DataTypes.STRING(140),
-   allowNull: false
-}
+const COL_TITLE_DEF = {
+  //general column title
+  type: Sequelize.DataTypes.STRING(140),
+  allowNull: false,
+};
 
-const COL_USERNAME_DEF = {                //general column username
-   type: Sequelize.DataTypes.STRING(30),
-   unique: true,
-   allowNull: false
-}
+const COL_USERNAME_DEF = {
+  //general column username
+  type: Sequelize.DataTypes.STRING(30),
+  unique: true,
+  allowNull: false,
+};
 
-const Users = db.define('user', {          //users table
-   id: COL_ID_DEF,
-   username: COL_USERNAME_DEF
-})
+const Users = db.define("user", {
+  //users table
+  id: COL_ID_DEF,
+  username: COL_USERNAME_DEF,
+});
 
-const Articles = db.define('article', {    //articles table
-   id: COL_ID_DEF,
-   title: COL_TITLE_DEF,
-   body: {
-      type: Sequelize.DataTypes.TEXT,
-      allowNull: false
-   }
-})
+const Articles = db.define("article", {
+  //articles table
+  id: COL_ID_DEF,
+  title: COL_TITLE_DEF,
+  body: {
+    type: Sequelize.DataTypes.TEXT,
+    allowNull: false,
+  },
+});
 
-const Comments = db.define('comment', {    //comments table
-   id: COL_ID_DEF,
-   title: COL_TITLE_DEF,
-   body: {
-      type: Sequelize.DataTypes.TEXT('tiny')
-   }
-})
+const Comments = db.define("comment", {
+  //comments table
+  id: COL_ID_DEF,
+  title: COL_TITLE_DEF,
+  body: {
+    type: Sequelize.DataTypes.TEXT("tiny"),
+  },
+});
 
 //setting up a relation between users, articles and comments
-Users.hasMany(Articles)
-Articles.belongsTo(Users)
+Users.hasMany(Articles);
+Articles.belongsTo(Users);
 
-Users.hasMany(Comments)
-Comments.belongsTo(Users)
+Users.hasMany(Comments);
+Comments.belongsTo(Users);
 
-Articles.hasMany(Comments)
-Comments.belongsTo(Articles)
+Articles.hasMany(Comments);
+Comments.belongsTo(Articles);
 
 module.exports = {
-   db, Users, Articles, Comments
-}
+  db,
+  Users,
+  Articles,
+  Comments,
+};

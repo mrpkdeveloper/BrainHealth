4f1586167a56dae3e49151352bb000c88b2d934d
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2022-01-12 22:04:24 +0530

    ui changes

--- src/public/app/common.js
+++ src/public/app/common.js
@@ -23,16 +23,16 @@ function loginIfNeeded() {
   }
 }
 
-function loginIfNeeded() {
-  //   if window.localstorage.user is present then pass JSON.parse(window.localStorage.user) to window.currentuser ///otherwise pass null
-  window.currentuser = window.localStorage.user
-    ? JSON.parse(window.localStorage.user)
-    : null;
-  if (!currentuser) {
-    // do login ... extract details from backend ... suppose extrated details in as json opbject named user
-    window.localStorage.user = JSON.stringify(user);
-    currentuser = user;
-  } else {
-    console.log("resuming session as ", currentuser.username);
-  }
-}
+// function loginIfNeeded() {
+//   //   if window.localstorage.user is present then pass JSON.parse(window.localStorage.user) to window.currentuser ///otherwise pass null
+//   window.currentuser = window.localStorage.user
+//     ? JSON.parse(window.localStorage.user)
+//     : null;
+//   if (!currentuser) {
+//     // do login ... extract details from backend ... suppose extrated details in as json opbject named user
+//     window.localStorage.user = JSON.stringify(user);
+//     currentuser = user;
+//   } else {
+//     console.log("resuming session as ", currentuser.username);
+//   }
+// }
